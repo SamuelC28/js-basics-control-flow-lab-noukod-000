67dc54b2d132812038f3b75eaa83e10754c976bb
Samuel Cornet <samuel@noukod.com>
2019-01-30 19:27:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function scuberGreetingForFeet(ride) {
        else if (ride > 2500) {
          result = 'No can do.';
        }
-       else if (someValue > 2000) {
+       else if (ride > 2000) {
          result = 'I will gladly take your thirty bucks.';
        }
 
